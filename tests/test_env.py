4e8fa7f5d8f2f3330b8784faff758dd91d2c11c1
Kaletsidik Ayalew <alexkalalw@gmail.com>
2025-05-25 11:34:53 +0300

    Configure in the .env

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,1 +1,7 @@
- 
+import sys
+sys.path.append(r"D:\@icog_projects\personalized-financial-assistant\config")  
+from config import GEMINI_API_KEY, DB_CONFIG, FILE_PATHS
+
+print("Gemini API Key:", GEMINI_API_KEY)
+print("Database Config:", DB_CONFIG)
+print("File Paths:", FILE_PATHS)
